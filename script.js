3c741f4016f6ba1434eaf14763a03156d3b1d448
NOMHWANGE <afathelion@gmail.com>
2023-06-25 12:26:31 +0300

    version1 on my API enabled search engine

---
+++ script.js
@@ -0,0 +1,51 @@
+
+
+//create the variable for the elements in index.html
+const searchForm = document.getElementById('search-form');
+const searchBox = document.getElementById('search-box');
+const searchResult= document.getElementById('search-result');
+const showMore = document.getElementById('show-more-btn');
+
+//get the API from where to obtain the images
+
+let keyword = '';
+let page = 1;
+
+const accesskey = 'L-w_o09yE3rV7_9L_RaLDDvjPS9k7dYlt2DHI_k_NGU'
+
+async function searchImages(){
+  keyword = searchBox.value;
+  const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accesskey}&per_page=12`
+
+  const response = await fetch(url);
+  const data = await response.json();
+
+  if(page ===1) {
+    searchResult.innerHTML = " ";
+  }
+
+  const results = data.results;
+  results.map ((result) => {
+    const image = document.createElement('img');
+    image.src = result.urls.small;
+    const imageLink = document.createElement('a');
+    imageLink.href = result.links.html;
+    imageLink.target = "_blank";
+    imageLink.appendChild(image);
+    searchResult.appendChild(imageLink)
+  });
+ 
+}
+showMoreBtn.style.display = "block";
+
+searchForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    page = 1;
+    searchImages();
+
+})
+
+showMoreBtn.addEventListener('click', () => {
+  page++;
+  searchImages();
+})
